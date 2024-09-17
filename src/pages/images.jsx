07adf35961394c3f5e5c01dbd00abb47a80735d7
Huynh <raphaelle.huynh@outlook.fr>
2024-09-17 23:52:50 +0200

    fix: photo

--- src/pages/images.jsx
+++ src/pages/images.jsx
@@ -1,18 +1,15 @@
-import img1 from "../assets/img1.png";
+import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg"
 import img4 from "../assets/img4.jpg";
 import img5 from "../assets/img5.jpg";
+import img6 from "../assets/img6.jpg";
 import img7 from "../assets/img7.jpg";
 import img8 from "../assets/img8.jpg";
-import img9 from "../assets/img9.png";
-import img12 from "../assets/img12.jpg";
-import img13 from "../assets/img13.png";
-import img14 from "../assets/img14.jpg";
 
 function Images() {
     const images = [
-        img1, img2, img4, img5, img7, 
-        img8, img9, img12, img13, img14
+        img1, img2, img4, img5, img6, img7, 
+        img8,
     ];
 
     return (
